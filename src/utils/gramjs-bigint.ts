1f25b81a50784b49fb7a22f2ef0ad24e8f078c11
itsdeadcow <do.koenig@yahoo.com>
2026-02-26 00:45:57 +0100

    bugfix: fix gramjs randomLong

--- src/utils/gramjs-bigint.ts
+++ src/utils/gramjs-bigint.ts
@@ -1,9 +1,10 @@
 import { randomBytes } from "crypto";
-import bigInt, { type BigInteger } from "big-integer";
+import { type BigInteger } from "big-integer";
+import { returnBigInt } from "telegram/Helpers.js";
 
 /** Convert native bigint or number to BigInteger for GramJS TL long fields */
 export function toLong(value: bigint | number): BigInteger {
-  return bigInt(String(value));
+  return returnBigInt(String(value));
 }
 
 /** Generate cryptographically random BigInteger for randomId / poll ID fields */
